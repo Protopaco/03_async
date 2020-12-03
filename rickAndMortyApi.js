42d17aed9440a8a53bc980e51103a9d67762dc90
Paul Stevens <twinkisan@hotmail.com>
2020-12-02 18:09:52 -0800

    refactored getCharacter using async/await

--- rickAndMortyApi.js
+++ rickAndMortyApi.js
@@ -1,17 +1,22 @@
 const fetch = require('node-fetch');
 
 url = "https://rickandmortyapi.com/api/character/"
-const getCharacter = (characterId) => {
-    return fetch(url + characterId)
-        .then((returnedObject) => {
-            return returnedObject.json()
-        })
-        .then((object) => {
-            const { name, status, species } = object
-            return { name, status, species }
+// const getCharacter = (characterId) => {
+//     return fetch(url + characterId)
+//         .then((returnedObject) => {
+//             return returnedObject.json()
+//         })
+//         .then((object) => {
+//             const { name, status, species } = object
+//             return { name, status, species }
 
-        })
+//         })
 
+// }
+
+const getCharacter = async (characterId) => {
+    const returnedObject = await fetch(url + characterId);
+    const { name, status, species } = await returnedObject.json();
+    return { name, status, species };
 }
-
 module.exports = { getCharacter }
