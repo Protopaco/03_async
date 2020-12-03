ee3bdd2246f1413e2f651140f0ef43ab26b4aea6
Paul Stevens <twinkisan@hotmail.com>
2020-12-02 18:33:10 -0800

    adds getManyCharacters() test

--- rickAndMortyApi.test.js
+++ rickAndMortyApi.test.js
@@ -2,12 +2,33 @@ const fetch = require('node-fetch');
 const { getCharacter } = require('./rickAndMortyApi.js')
 
 
-characterId = 1;
-expectedResult = {
+const characterId = 1;
+const expectedResult = {
     name: 'Rick Sanchez',
     status: 'Alive',
     species: 'Human'
 }
+
+const idArray = [1, 2, 3];
+expectedArrayResult[
+    {
+        name: 'Rick Sanchez',
+        status: 'Alive',
+        species: 'Human'
+    },
+    {
+        name: 'Morty Smith',
+        status: 'Alive',
+        species: 'Human'
+    },
+    {
+        name: 'Summer Smith',
+        status: 'Alive',
+        species: 'Human'
+    },
+]
+
+
 
 describe('tests getCharacter', () => {
 
@@ -17,6 +38,13 @@ describe('tests getCharacter', () => {
                 expect(expectedResult).toEqual(result)
             })
     })
+})
 
-
+describe('tests getManyCharacters', () => {
+    it('calls getManyCharacters, and expects to see name, status & species for each', () => {
+        return getManyCharacters(idArray)
+            .then((result) => {
+                expect(expectedArrayResult).toEqual(result)
+            })
+    })
 })
